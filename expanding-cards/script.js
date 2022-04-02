b8841cd325aa747354828fcba4bc0e4044451239
AdamAKw <kwiatkowskiadam1ta@gmail.com>
2022-04-02 22:20:29 +0200

    add key moving

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,11 +1,49 @@
 const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) =>
-  panel.addEventListener("click", (e) => {
+  panel.addEventListener("click", () => {
     removeActiveClass();
-    e.target.classList.add("active");
+    panel.classList.add("active");
   })
 );
+
+document.addEventListener("keydown", (e) => {
+  if (!e.repeat) {
+    moveActiveClassByIndex(getLeftIndex(e.key));
+    moveActiveClassByIndex(getRigthIndex(e.key));
+  }
+});
+
+function moveActiveClassByIndex(indexToChange) {
+  if (indexToChange !== undefined) {
+    removeActiveClass();
+    panels[indexToChange].classList.add("active");
+  }
+}
+
+function getLeftIndex(direction) {
+  if (direction === "ArrowLeft") {
+    for (const [index, panel] of panels.entries()) {
+      if (index !== 0 && isActive(panel)) {
+        return index - 1;
+      }
+    }
+  }
+}
+
+function getRigthIndex(direction) {
+  if (direction === "ArrowRight") {
+    for (const [index, panel] of panels.entries()) {
+      if (index !== panels.length - 1 && isActive(panel)) {
+        return index + 1;
+      }
+    }
+  }
+}
+
+function isActive(panel) {
+  return panel.classList.contains("active");
+}
 
 function removeActiveClass() {
   panels.forEach((panel) => panel.classList.remove("active"));
