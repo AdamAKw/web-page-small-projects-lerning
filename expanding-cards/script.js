d4ef38ebf8b7be2a89781b7e9810d1475ce61e50
AdamAKw <kwiatkowskiadam1ta@gmail.com>
2022-04-02 20:17:09 +0200

    Expanding cards mini project

---
+++ expanding-cards/script.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) =>
+  panel.addEventListener("click", (e) => {
+    removeActiveClass();
+    e.target.classList.add("active");
+  })
+);
+
+function removeActiveClass() {
+  panels.forEach((panel) => panel.classList.remove("active"));
+}
